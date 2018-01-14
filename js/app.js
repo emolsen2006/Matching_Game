7815d03c57e6939922c7225b78b51907a733cb3b
Eric_Olsen <emolsen2006@yahoo.com>
2018-01-14 08:19:57 +0300

    Feat: Event listeners added, logical compare built

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-//build initial list of unique cards
+//statically build the inner HTML of the deck
 var cards = ['<i class="fa fa-motorcycle fa-lg"></i>',
             '<i class="fa fa-motorcycle fa-lg"></i>',
             '<i class="fa fa-truck fa-lg"></i>',
@@ -17,8 +17,63 @@ var cards = ['<i class="fa fa-motorcycle fa-lg"></i>',
             '<i class="fa fa-wheelchair fa-lg"></i>',
             '<i class="fa fa-wheelchair fa-lg"></i>'];
 
+//our face-up array for matching
+var faceUp = [];
 
-// Shuffle function, modified from http://stackoverflow.com/a/2450976
+//deal the cards
+deal(cards);
+
+//shuffles, sets innerHTML, and adds mouse EventListener
+function deal(cards) {
+  //shuffle the deck
+  shuffle(cards);
+  //beccause of the DOM lookup, the first card is special
+  var firstCard = document.querySelector('.deck').firstElementChild;
+  firstCard.innerHTML = cards[0];
+  makeClickable(firstCard);
+  var nextCard = firstCard
+
+  //iterate through the deck by 'next sibling'
+  for (var i = 1; i < cards.length; i++) {
+    nextCard = nextCard.nextElementSibling;
+    nextCard.innerHTML = cards[i];
+    makeClickable(nextCard);  //method to add event listeners
+  }
+}
+
+//alternates between CSS classes to show sides of cards and animation
+function makeClickable(card) {
+  //TODO: user should only click to show, not both
+  card.addEventListener('click', function(){
+    if (card.className === 'card'){
+      //TODO: add to the open card list
+      card.className = 'card show';
+      compare(card);
+    } else {
+      card.className = 'card';
+    }
+  });
+}
+
+function compare(card) {
+  //if empty or even numbered
+  if (faceUp.length === 0 || (faceUp.length % 2 ===0)) {
+    faceUp.push(card);
+    // console.log(card);
+    // console.log(faceUp.value);
+  } else {
+    for (var i = 0; i < faceUp.length; i++) {
+      if (card.innerHTML === faceUp[i].innerHTML) {
+        console.log('match');
+      } else {
+        console.log('mismatch');
+      }
+    }
+  }
+
+}
+
+// Shuffle function, slightly modified from http://stackoverflow.com/a/2450976
 function shuffle(array) {
   var currentIndex = array.length;
   var temp;
@@ -32,21 +87,3 @@ function shuffle(array) {
   }
   return array;
 }
-
-//shuffle the deck
-shuffle(cards);
-
-function deal(cards) {
-  var firstCard = document.querySelector('.deck').firstElementChild;
-  firstCard.innerHTML = cards[0];
-  var nextCard = firstCard
-
-  for (var i = 1; i < cards.length; i++) {
-    nextCard = nextCard.nextElementSibling;
-    nextCard.innerHTML = cards[i];
-    console.log(nextCard);
-  }
-}
-
-//deal the cards
-deal(cards);
