1d3c4b5b4f0ae46ac11f5ea0c3061a67db25c898
Eric_Olsen <emolsen2006@yahoo.com>
2018-01-15 07:40:53 +0300

    Feat: Added reset button and CSS flip effect

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 
 var moveCounter = document.querySelector('.moves');
 var moves = 0;  //move counter
+var firstCard, secondCard = true; //control measure for fast clickers
+var faceUp = []; //our face-up array for matching
+
 //statically built the inner HTML of the deck
 var cards = ['<i class="fa fa-motorcycle fa-lg"></i>',
             '<i class="fa fa-motorcycle fa-lg"></i>',
@@ -19,25 +22,26 @@ var cards = ['<i class="fa fa-motorcycle fa-lg"></i>',
             '<i class="fa fa-wheelchair fa-lg"></i>',
             '<i class="fa fa-wheelchair fa-lg"></i>'];
 
-//our face-up array for matching
-var faceUp = [];
+
 
 //deal the cards
 deal(cards);
 
 //shuffles, sets innerHTML, and adds mouse EventListener
 function deal(cards) {
-  //shuffle the deck
-  shuffle(cards);
+  moves = 0; //reset move counter
+  shuffle(cards); //shuffle function below
   //beccause of the DOM lookup, the first card is special
   var firstCard = document.querySelector('.deck').firstElementChild;
   firstCard.innerHTML = cards[0];
+  firstCard.className = 'card'; //used during the reset button
   makeClickable(firstCard);
   var nextCard = firstCard
 
   //iterate through the deck by 'nextElementSibling'
   for (var i = 1; i < cards.length; i++) {
     nextCard = nextCard.nextElementSibling;
+    nextCard.className = 'card';  //used during the rest button
     nextCard.innerHTML = cards[i];
     makeClickable(nextCard);  //add event listeners with compare function
   }
@@ -45,55 +49,50 @@ function deal(cards) {
 
 //alternates between CSS classes to show sides of cards and animation
 function makeClickable(card) {
-  //TODO: user should only click to show, not both
   card.addEventListener('click', function(){
     if (card.className === 'card'){
-      card.className = 'card show';
-      //add comparison at time of event listen
-      compare(card);
-      //TODO: throw in sleep command for the player
+      if(firstCard || secondCard){    //locks card flip when checking for match
+        card.className = 'card show'; //flips the card for display
+        compare(card); //comparison and main game logic
+      }
     } else {
-      //delete this once you fix the game logic
       //displays when a showing card is clicked again
-      console.log('error');
+      console.log('already flipped');
     }
   });
 }
 
-//game logic:
-//Overall: This is iterative and launched on each card mouse click
-//1. If even, puts the card in the faceUp stack
-//2. Otherwise, peaks the stack and compares innerHTML
-//3. If they match, push the card to stackoverflow
-//4. If they don't match, pop the stack
-//5. In both outcomes, increment the moves counter
-//6. Check if the stack === 16, if so, player wins, if not, continue
 function compare(card) {
-  if (faceUp.length === 0 || faceUp.length % 2 === 0) {
-    //pushes first card for comparison into the array
-    faceUp.push(card);
+  //Step 1: Flip an initial card for comparison
+  if (faceUp.length === 0 || faceUp.length % 2 === 0) { //is empty or even
+    faceUp.push(card);  //pushes first card for comparison into the array
+    firstCard = false;  //lock the first card for earlier boolean check
   } else {
-    //takes the newly selected card and compares to the last card
     //credit to stackoverflow at: https://stackoverflow.com/questions/1141302
-    setTimeout(function(){ //should delay comparison by 1 sec
-      if (faceUp[faceUp.length - 1].innerHTML === card.innerHTML){
-        console.log('match');
-        faceUp.push(card);
+    //Step 2: Flip other card to compare; cards match
+    secondCard = false; //lock card so impatient players don't mess up the logic
+    setTimeout(function(){ //delay the flipping by one second for human processing
+      if (faceUp[faceUp.length - 1].innerHTML === card.innerHTML){  // both cards match
+        // card.className = 'card match';
+        faceUp.push(card);  //push newly matched card onto array
+        //if array equals 16, declare victory for player
+        if (faceUp.length === 16) {
+          alert('winner!!!');
+        }
+        firstCard, secondCard = true; //unlock the first and second card
       } else {
-        console.log('no match');
-        //TODO: sleep for 1 second...much harder in Javascript than I thought
-
-        card.className = 'card';
-        faceUp[faceUp.length - 1].className = 'card';
-        faceUp.pop();
+        //Step 3: Cards don't match and both are flipped back over
+        //TODO: add CSS animation class
+        card.className = 'card'; //flips the card back over
+        faceUp[faceUp.length - 1].className = 'card'; //flip card over
+        faceUp.pop(); //remove from array for new comparison
+        firstCard, secondCard = true; //unlock cards
       }
-    }, 1000);
-    moves++;
+    }, 1000); //1,000 delays card flip by 1 second
+    moves++;  //increment move counter, works for matches and mis-matches
   }
-  moveCounter.innerHTML = moves;
-  console.log(moves);
+  moveCounter.innerHTML = moves;  //update HTML tag with new moves
 }
-
 
 // Shuffle function, slightly modified from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -109,3 +108,11 @@ function shuffle(array) {
   }
   return array;
 }
+
+//set reset event
+var reset = document.getElementById('restart')
+reset.addEventListener('click', function(){
+  deal(cards);  //re-deal the hands
+  moveCounter.innerHTML = 0;  //reset the move counter
+  //TODO: reset the game timer when built
+});
